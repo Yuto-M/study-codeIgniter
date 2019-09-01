5711893359d928c2cea5b4f19bbf316ba976c22d
宮川優斗 <miyakaway7@gmail.com>
2019-09-01 13:04:02 +0900

    phpcsテスト

--- web/application/controllers/News.php
+++ web/application/controllers/News.php
@@ -1,7 +1,7 @@
 <?php
 class News extends CI_Controller
 {
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->load->model('news_model');
